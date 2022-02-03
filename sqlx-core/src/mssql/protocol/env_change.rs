c17b2d46097f49ab30d7e904abc016bb09626d24
Jack Sibley <33719258+Jack-Sibley@users.noreply.github.com>
2022-02-03 16:20:48 +0000

    undo that

--- sqlx-core/src/mssql/protocol/env_change.rs
+++ sqlx-core/src/mssql/protocol/env_change.rs
@@ -57,8 +57,6 @@ impl EnvChange {
                 EnvChange::RollbackTransaction(data.get_u64_le())
             }
 
-            20 => EnvChange::RoutingInformation,
-
             _ => {
                 return Err(err_protocol!("unexpected value {} for ENVCHANGE Type", ty));
             }
